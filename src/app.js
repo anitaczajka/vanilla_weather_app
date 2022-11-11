b668ab448af4fc819806604a9a361c5cf188dd22
Anita <anita.czajka.91@outlook.com>
2022-11-11 11:43:25 +0100

    showing the actual temperature and descriptions

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+
+  let windElement = document.querySelector("#windSpeed");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "4f5b6ff5t50992bb0a3f47b106ba0o47";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
